4a68b52586697f58b8f6f09c7a0d602c7deb3c7b
Tim Jensen <timothy.r.jensen@boeing.com>
2018-12-22 17:07:34 -0500

    commit

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TodoApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ToDoApi
 {
@@ -23,11 +24,35 @@ namespace ToDoApi
             services.AddDbContext<TodoContext>(opt =>
                 opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //////////////////////////////////////////////////////////////////////////
+            /// Added this section.  See:
+            /// https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-2.2&tabs=visual-studio
+            /// 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvc();
+            //////////////////////////////////////////////////////////////////////////
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -42,5 +67,7 @@ namespace ToDoApi
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+
+
     }
 }
